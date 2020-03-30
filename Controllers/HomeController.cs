22c08d85e65ba22d006f52abf4d237d66339ab9c
DixonCh <dixonchaudhary7@gmail.com>
2020-03-30 12:16:44 +0545

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using CRUDOPERATION.Models;
+using CRUDOPERATION.Repository;
+
+namespace CRUDOPERATION.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ICourseRepository repo = new CourseRepository();
+        public ActionResult Index()
+        {
+
+
+            return View(repo.GetAll());
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                int result =repo.Insert(new Course()
+                {
+                    Name=cvm.Name,
+                    Fees=cvm.Fees,
+                    Status=cvm.Status
+                });
+                if(result > 0)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Error = "Unable to Insert";
+                }
+            }
+            return View(cvm);
+        }
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            int cId = id ?? 0;
+            //if its not null then assign 0 value
+            Course course = repo.Find(cId);
+            if (course == null)
+            {
+                return RedirectToAction("Index");
+            }
+            
+            return View(new CourseViewModel()
+            {
+                Id=course.Id,
+                Name=course.Name,
+                Fees=course.Fees,
+                Status=course.Status
+
+
+
+            });
+        }
+        [HttpPost]
+        public ActionResult Edit(CourseViewModel cvm)
+        {
+            if (ModelState.IsValid)
+            {
+                repo.Update(new Course()
+                {
+                    Id=cvm.Id,
+                    Name=cvm.Name,
+                    Fees=cvm.Fees,
+                    Status=cvm.Status
+
+
+                });
+                return RedirectToAction("Index");
+            }
+            return View(cvm);
+        }
+
+        }
+}
---
+++ DbUtility/DbConnection.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Configuration;
+
+namespace CRUDOPERATION.DbUtility
+{
+    public class DbConnection
+    {
+        private SqlConnection conn;
+        private SqlCommand cmd;
+        public void Open()
+        {
+            conn = new SqlConnection();
+           
+            conn.ConnectionString = ConfigurationManager.ConnectionStrings["connString"].ConnectionString;
+            conn.Open();
+        }
+
+
+        public void InitCommand(string sql, CommandType type)
+        {
+            cmd= new SqlCommand();
+            cmd.CommandText = sql;
+            cmd.CommandType = type;
+            cmd.Connection = conn;
+        }
+
+        public SqlDataReader ExecuteReader()
+        {
+          
+            return cmd.ExecuteReader();
+
+        }
+        public void AddInputParameter(DbType type, string pName, object pValue)
+        {
+            cmd.Parameters.Add(new SqlParameter()
+            {
+                DbType = DbType.String,
+                ParameterName = pName,
+                Value = pValue
+            });
+        }
+        public int ExecuteNonQuery()
+        {
+            return cmd.ExecuteNonQuery();
+
+        }
+        public void Close()
+        {
+            if(conn!=null && conn.State != ConnectionState.Closed)
+            {
+                conn.Close();
+                conn = null;
+            }
+        }
+    }
+}
